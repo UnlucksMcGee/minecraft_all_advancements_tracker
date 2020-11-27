961673276cf580c2d4adfa2c6fd9e9929e9b1fd1
UnlucksMcGee <74118470+UnlucksMcGee@users.noreply.github.com>
2020-11-27 23:38:21 +0200

    Added error checking for invalid credentials.json file

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import datetime
+import json
 import os
 import pickle
+import sys
 import time
 import tkinter as tk
 
@@ -11,6 +13,17 @@ from googleapiclient.discovery import build
 
 import gui
 
+def get_new_credentials():
+    try:
+        SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+        flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+        return flow.run_local_server(port=0)
+    except (json.decoder.JSONDecodeError, ValueError):
+        err_msg = f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Failure to read credentials.json file.\nTry downloading it again."
+        print(err_msg)
+        with open("debug_log.txt", "a") as f:
+            f.write(err_msg+"\n")
+        sys.exit(1)
 
 def get_service():
     creds = None
@@ -22,15 +35,9 @@ def get_service():
             try:
                 creds.refresh(Request())
             except:
-                SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
-                creds = flow.run_local_server(port=0)
+                creds = get_new_credentials()
         else:
-            SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = get_new_credentials()
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
